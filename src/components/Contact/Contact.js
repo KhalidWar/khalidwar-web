255edfa5e37cf7c944aaf403604a7276f262486f
Khalid Warsame <dev@khalidwar.com>
2022-01-24 01:09:21 -0600

    Updated Contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { TwitterIcon } from "../Landing/components/SocialIcon";
+import { twitterLink } from "../../data/SocialLinks";
 import { Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import emailjs from "@emailjs/browser";
@@ -61,71 +63,77 @@ function Contact() {
   return (
     <section id="contact">
       <div className="container">
-          <div className="contact-us">
-            <ToastContainer
-              position="bottom-left"
-              autoClose={3000}
-              closeOnClick
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-            />
-            <h2>Get in touch!</h2>
-            <p>Contract me for your projects.</p>
-          </div>
-          <div className="contact-form">
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              label="Full Name"
-              name="name"
-              required
-              error={showError && !formData.name}
-              helperText={showError && !formData.name && "Name is required"}
-              value={formData.name}
-              onChange={updateForm}
-            />
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              label="Email"
-              name="email"
-              required
-              error={
-                (showError && !formData.email) || (showError && !isEmailValid)
-              }
-              helperText={
-                (showError && !formData.email && "Email is required") ||
-                (showError && !isEmailValid && "Email is invalid")
-              }
-              value={formData.email}
-              onChange={updateForm}
-            />
-            <TextField
-              className="message-input"
-              id="outlined-multiline-static"
-              label="Message"
-              name="message"
-              multiline
-              rows={4}
-              required
-              error={showError && !formData.message}
-              helperText={
-                showError && !formData.message && "Message is required"
-              }
-              value={formData.message}
-              onChange={updateForm}
-            />
-            <Button
-              className="submit-button"
-              variant="contained"
-              color="error"
-              size="large"
-              onClick={sendMessage}
-            >
-              Submit
-            </Button>
-          </div>
+        <div className="contact-us">
+          <ToastContainer
+            position="bottom-left"
+            autoClose={3000}
+            closeOnClick
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          <p>Have a great idea?</p>
+          <h3>Shoot me a message and let's talk!</h3>
+          <TwitterIcon
+            whileHover={{ scale: 1.3 }}
+            whileTap={{ scale: 0.8 }}
+            className="twitter-icon"
+            onClick={() => {
+              window.open(twitterLink, "_blank");
+            }}
+          />
+        </div>
+        <div className="contact-form">
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            label="Full Name"
+            name="name"
+            required
+            error={showError && !formData.name}
+            helperText={showError && !formData.name && "Name is required"}
+            value={formData.name}
+            onChange={updateForm}
+          />
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            label="Email"
+            name="email"
+            required
+            error={
+              (showError && !formData.email) || (showError && !isEmailValid)
+            }
+            helperText={
+              (showError && !formData.email && "Email is required") ||
+              (showError && !isEmailValid && "Email is invalid")
+            }
+            value={formData.email}
+            onChange={updateForm}
+          />
+          <TextField
+            className="message-input"
+            id="outlined-multiline-static"
+            label="Message"
+            name="message"
+            multiline
+            rows={4}
+            required
+            error={showError && !formData.message}
+            helperText={showError && !formData.message && "Message is required"}
+            value={formData.message}
+            onChange={updateForm}
+          />
+          <Button
+            className="submit-button"
+            variant="contained"
+            color="error"
+            size="large"
+            onClick={sendMessage}
+          >
+            Submit
+          </Button>
+        </div>
       </div>
     </section>
   );
