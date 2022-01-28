d9197d5fc91504fe46f02999191475ec2b21ef9a
Khalid Warsame <dev@khalidwar.com>
2022-01-28 15:26:47 -0600

    Reordered App components

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <ModelViewer />
       <NavBar />
+      <ModelViewer />
       <About />
       <Experience />
       <Portfolio />
