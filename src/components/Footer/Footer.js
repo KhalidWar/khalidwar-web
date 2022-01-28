52e9775d7c18e985d4b8d26c69b5c31de10c9104
Khalid Warsame <dev@khalidwar.com>
2022-01-27 23:05:46 -0600

    Added Cube credits to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,20 @@ import "./Footer.css";
 function Footer() {
   const currentYear = new Date().getFullYear();
 
+  const cube = "https://skfb.ly/o8zEX";
+
   return (
     <section id="footer">
       <div className="container">
         <p>Copyright © {currentYear} Khalid Warsame</p>
+
+        <p
+          onClick={() => {
+            window.open(cube, "blank");
+          }}
+        >
+          "Cube Cascade" by Tycho Magnetic Anomaly
+        </p>
       </div>
     </section>
   );
