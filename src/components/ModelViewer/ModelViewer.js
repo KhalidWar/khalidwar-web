a61886b7251538a97e8378f6b97ec26f99640211
Khalid Warsame <dev@khalidwar.com>
2022-01-28 10:33:48 -0600

    Added controls to 3D model

--- src/components/ModelViewer/ModelViewer.js
+++ src/components/ModelViewer/ModelViewer.js
@@ -1,12 +1,40 @@
-import React, { Suspense } from "react";
+import React, { useState, Suspense } from "react";
 import IconButton from "@mui/material/IconButton";
 import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 import GltfModel from "./components/GltfModel";
 import LoadingIndicator from "./components/LoadingIndicator";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { Switch } from "@mui/material";
 
 import "./ModelViewer.css";
 
 export default function ModelViewer() {
+  const [showControlls, setShowControlls] = useState(false);
+  const [autoRotate, setAutoRotate] = useState(true);
+  const [colors, setColors] = useState({
+    red: true,
+    blue: true,
+    green: true,
+    yellow: true,
+    white: false,
+  });
+
+  const handleColorChange = (event) => {
+    const { name, checked } = event.target;
+    setColors({
+      ...colors,
+      [name]: checked,
+    });
+  };
+
+  const handleAutoRotate = (event) => {
+    setAutoRotate(event.target.checked);
+  };
+
+  const handleControlChange = (event) => {
+    setShowControlls(event.target.checked);
+  };
+
   function goToAboutSection() {
     const anchor = document.querySelector("#about");
     anchor.scrollIntoView({ behavior: "smooth", block: "center" });
@@ -15,8 +43,119 @@ export default function ModelViewer() {
   return (
     <div id="model">
       <Suspense fallback={<LoadingIndicator />}>
-        <GltfModel />
+        <GltfModel
+          red={colors.red}
+          blue={colors.blue}
+          green={colors.green}
+          yellow={colors.yellow}
+          white={colors.white}
+          autoRotate={autoRotate}
+        />
       </Suspense>
+      <div
+        className="controls"
+        style={{
+          backgroundColor: !showControlls && "transparent",
+        }}
+      >
+        {showControlls && (
+          <FormControlLabel
+            label="Red"
+            labelPlacement="start"
+            control={
+              <Switch
+                color="primary"
+                name="red"
+                checked={colors.red}
+                onChange={handleColorChange}
+              />
+            }
+          />
+        )}
+
+        {showControlls && (
+          <FormControlLabel
+            label="Blue"
+            labelPlacement="start"
+            control={
+              <Switch
+                color="primary"
+                name="blue"
+                checked={colors.blue}
+                onChange={handleColorChange}
+              />
+            }
+          />
+        )}
+        {showControlls && (
+          <FormControlLabel
+            label="Green"
+            labelPlacement="start"
+            control={
+              <Switch
+                color="primary"
+                name="green"
+                checked={colors.green}
+                onChange={handleColorChange}
+              />
+            }
+          />
+        )}
+        {showControlls && (
+          <FormControlLabel
+            label="Yellow"
+            labelPlacement="start"
+            control={
+              <Switch
+                color="primary"
+                name="yellow"
+                checked={colors.yellow}
+                onChange={handleColorChange}
+              />
+            }
+          />
+        )}
+        {showControlls && (
+          <FormControlLabel
+            label="White"
+            labelPlacement="start"
+            control={
+              <Switch
+                color="primary"
+                name="white"
+                checked={colors.white}
+                onChange={handleColorChange}
+              />
+            }
+          />
+        )}
+
+        {showControlls && (
+          <FormControlLabel
+            label="Rotate"
+            labelPlacement="start"
+            control={
+              <Switch
+                color="primary"
+                checked={autoRotate}
+                onChange={handleAutoRotate}
+              />
+            }
+          />
+        )}
+
+        <FormControlLabel
+          label="Controls"
+          labelPlacement="start"
+          control={
+            <Switch
+              color="primary"
+              checked={showControlls}
+              onChange={handleControlChange}
+            />
+          }
+        />
+      </div>
 
       <div className="icon">
         <IconButton color="inherit" onClick={goToAboutSection}>
--- src/components/ModelViewer/components/GltfModel.js
+++ src/components/ModelViewer/components/GltfModel.js
@@ -3,6 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import WhiteLights from "./WhiteLights";
 
 import "../ModelViewer.css";
 
@@ -13,43 +14,13 @@ export default function GltfModel(props) {
     <Canvas className="canvas">
       <primitive object={earthGltf.scene} position={[0, 0, 0]} />
 
-      <directionalLight
-        color="white"
-        distance={5}
-        intensity={3}
-        position={[0, 2, 0]}
-      />
-      <directionalLight
-        distance={5}
-        intensity={3}
-        position={[0, 0, 2]}
-        color="yellow"
-      />
-      <directionalLight
-        distance={5}
-        intensity={3}
-        position={[0, 0, -2]}
-        color="blue"
-      />
-      <directionalLight
-        distance={5}
-        intensity={3}
-        position={[-2, 0, 0]}
-        color="#0EEC82"
-      />
-      <directionalLight
-        distance={5}
-        intensity={3}
-        position={[2, 0, 0]}
-        color="#ff005b"
-      />
       <OrbitControls
         enablePan={false}
         zoomSpeed={0.8}
         rotateSpeed={0.8}
         minDistance={0.234}
         maxDistance={4}
-        autoRotate={true}
+        autoRotate={props.autoRotate}
         autoRotateSpeed={0.8}
         target={earthGltf.target}
       />
@@ -60,6 +31,33 @@ export default function GltfModel(props) {
         factor={4}
         saturation={0}
         fade
+      />
+
+      <WhiteLights white={props.white} />
+
+      <directionalLight
+        color="yellow"
+        distance={5}
+        intensity={props.yellow ? 3 : 0}
+        position={[0, 0, 2]}
+      />
+      <directionalLight
+        color="blue"
+        distance={5}
+        intensity={props.blue ? 3 : 0}
+        position={[-2, 0, 0]}
+      />
+      <directionalLight
+        color="#0EEC82"
+        distance={5}
+        intensity={props.green ? 3 : 0}
+        position={[0, 0, -2]}
+      />
+      <directionalLight
+        color="#ff005b"
+        distance={5}
+        intensity={props.red ? 3 : 0}
+        position={[2, 0, 0]}
       />
     </Canvas>
   );
